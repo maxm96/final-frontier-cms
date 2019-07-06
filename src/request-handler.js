7120764c5e052e5685e530d044ebb6d0337478e4
Max McDaniel <max15@ksu.edu>
2019-07-06 20:12:31 +0000

    create parse-body module and refactor

--- src/request-handler.js
+++ src/request-handler.js
@@ -14,44 +14,56 @@ const serveError = require('./serve-error')
  * @param {http.serverResponse} res - the response object
  */
 module.exports = function requestHandler(req, res) {
-  // only accept get requests
-  if (req.method !== 'GET') {
-    serveError(null, 501, 'Not Implemented', res)
-    return
+  switch (req.url) {
+    case '/': // main page
+      serveMain(req, res, (err) => {
+        if (err)
+          serveError(err, 500, 'Server Error', res)
+      })
+      break
+    case '/admin':
+      // TODO
+      break
+    case '/create':
+      // TODO
+      break
+    default: // serve some other resource
+      serveResource(req, res)
   }
-  
-  if (req.url === '/') {
-    // serve main page
-    serveMain(req, res, (err) => serveError(err, 500, 'Server Error', res))
-  } else {
-    // not main page -- get pathname and invoke stat
-    var pathname = url.parse(req.url).pathname
-    var filePath = path.join('public', pathname)
-    
-    // decode file path to prevent errors from paths with spaces
-    filePath = decodeURIComponent(filePath)
-    
-    fs.stat(filePath, (err, stats) => {
-      if (err)
-        return serveError(err, 404, 'Not Found', res)
-      
-      if (stats.isFile()) { // serve file resource
-        if (req.headers.range) // stream if range is requested
-          streamMedia(filePath, stats, req, res)
-        else // else serve entire file
-          serveFile(filePath, res, (err) => {
-            if (err)
-              serveError(err, 500, 'Server Error', res)
-          })
-      } else if (stats.isDirectory()) { // serve index directory
-        /** TODO: index links are broken **/
-        serveIndex(filePath, res, (err) => {
+}
+
+/** @function serveResource
+ * Helper function to serve file, index, or media.
+ * @param {http.incomingMessage} req - the request object
+ * @param {http.serverResponse} res - the response object
+ */
+function serveResource(req, res) {
+  var pathname = url.parse(req.url).pathname
+  var filePath = path.join('public', pathname)
+
+  // decode file path to prevent errors from paths with spaces
+  filePath = decodeURIComponent(filePath)
+
+  fs.stat(filePath, (err, stats) => {
+    if (err)
+      return serveError(err, 404, 'Not Found', res)
+
+    if (stats.isFile()) { // serve file resource
+      if (req.headers.range) // stream if range is requested
+        streamMedia(filePath, stats, req, res)
+      else // else serve entire file
+        serveFile(filePath, res, (err) => {
           if (err)
             serveError(err, 500, 'Server Error', res)
         })
-      } else {
-        serveError(null, 404, 'Not Found', res)
-      }
-    })
-  }
+    } else if (stats.isDirectory()) { // serve index directory
+      /** TODO: index links are broken **/
+      serveIndex(filePath, res, (err) => {
+        if (err)
+          serveError(err, 500, 'Server Error', res)
+      })
+    } else {
+      serveError(null, 404, 'Not Found', res)
+    }
+  })
 }
